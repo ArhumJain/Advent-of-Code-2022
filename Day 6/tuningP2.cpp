1d31c17599a10ddb9792d3b3b2198d1596e082f7
ArhumJain <arhumjain9@hotmail.com>
2022-12-06 14:24:44 -0800

    Day 6

---
+++ Day 6/tuningP2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_set>
+
+using namespace std;
+using F = ifstream;
+
+constexpr int TEST = 0;
+F f(TEST == 1 ? "test.in" : "main.in");
+
+string buffer;
+int main() {
+  f >> buffer;
+  string code = buffer.substr(0,14);
+  for (int i=4; i<buffer.size(); i++) {
+      code.erase(code.begin()); code.push_back(buffer[i]);
+      if (unordered_set<char>(code.begin(), code.end()).size() == 14) { cout << i+1; break; }
+  }
+}
