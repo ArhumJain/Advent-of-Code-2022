911ab31dd2e397125cd0493713663d30c99672e4
ArhumJain <arhumjain10@gmail.com>
2022-12-07 11:04:01 -0800

    day 7 update

--- Day 7/deviceP1.cpp
+++ Day 7/deviceP1.cpp
@@ -5,6 +5,24 @@ using namespace aoc;
 constexpr int TEST = 1;
 F f(TEST == 1 ? "test.in" : "main.in");
 
+struct Directory {
+    vec<tuple<int, string>> files;
+    umap<string, Directory*> dirs;
+    umap<string, int> files;
+    string name;
+    Directory(string name) {this->name = name;}
+
+};
+
+string a;
 int main() {
+    Directory currDir("/");
+    bool listing = false;
+    while (true){
+        if (a == "$") {
+            listing = false;
+
+        }
+    }
 
 }
