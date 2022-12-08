191fb82cb76e73a387e78f862fbfb511bc4ae29e
Arhum Jain <arhumjain10@gmail.com>
2022-12-07 21:11:38 -0800

    day 7 finish

--- Day 7/deviceP1.cpp
+++ Day 7/deviceP1.cpp
@@ -2,27 +2,64 @@
 
 using namespace aoc;
 
-constexpr int TEST = 1;
+constexpr int TEST = 0;
 F f(TEST == 1 ? "test.in" : "main.in");
 
 struct Directory {
-    vec<tuple<int, string>> files;
-    umap<string, Directory*> dirs;
-    umap<string, int> files;
-    string name;
-    Directory(string name) {this->name = name;}
+	umap<string, Directory *> dirs;
+	umap<string, long> files;
+	string name;
+	int size = 0;
+	Directory *parent;
 
+	void increaseSize(int size) {
+		this->size += size;
+		if (parent != nullptr) this->parent->increaseSize(size);
+	}
+	Directory(string name, Directory *parent) {
+		this->name = name;
+		this->parent = parent;
+	}
 };
 
-string a;
+void sum(Directory *root, int &total) {
+	if (root->size <= 100000) total += root->size;
+	if (root->dirs.size() == 0) return;
+	for (auto i : root->dirs) {
+		sum(i.second, total);
+	}
+}
+
+string first;
+string second;
 int main() {
-    Directory currDir("/");
-    bool listing = false;
-    while (true){
-        if (a == "$") {
-            listing = false;
-
-        }
-    }
-
+	Directory *currDir = new Directory("/", nullptr);
+	Directory *root = currDir;
+	while (true) {
+		f >> first;
+		if (first == "$") {
+			f >> first;
+			if (first == "cd") {
+				f >> first;
+				if (first == "..") currDir = currDir->parent;
+				else {
+					if (currDir->dirs[first] == nullptr) currDir->dirs[first] = new Directory(first, currDir);
+					currDir = currDir->dirs[first];
+				}
+			}
+		}
+		else if (first == "dir") {
+			f >> first;
+			if (currDir->dirs[first] == nullptr) currDir->dirs[first] = new Directory(first, currDir);
+		}
+		else {
+			f >> second;
+			currDir->files[second] = stoi(first);
+			currDir->increaseSize(stoi(first));
+		}
+		if (f.eof()) break;
+	}
+	int total = 0;
+	sum(root, total);
+	cout << total;
 }
--- Day 7/deviceP2.cpp
+++ Day 7/deviceP2.cpp
@@ -2,9 +2,67 @@
 
 using namespace aoc;
 
-constexpr int TEST = 1;
+constexpr int TEST = 0;
 F f(TEST == 1 ? "test.in" : "main.in");
 
-int main() {
+struct Directory {
+	umap<string, Directory *> dirs;
+	umap<string, long> files;
+	string name;
+	int size = 0;
+	Directory *parent;
 
+	void increaseSize(int size) {
+		this->size += size;
+		if (parent != nullptr) this->parent->increaseSize(size);
+	}
+	Directory(string name, Directory *parent) {
+		this->name = name;
+		this->parent = parent;
+	}
+};
+
+void sum(Directory *root, Directory &minDir, int &needed) {
+	if (root->size >= needed && root->size < minDir.size) minDir = *(root);
+	if (root->dirs.size() != 0) {
+        for (auto i : root->dirs) {
+            sum(i.second, minDir, needed);
+        }
+    }
+}
+
+string first;
+string second;
+int main() {
+	Directory *currDir = new Directory("/", nullptr);
+	Directory *root = currDir;
+	bool listing = false;
+	while (true) {
+		f >> first;
+		if (first == "$") {
+			f >> first;
+			if (first == "cd") {
+				f >> first;
+				if (first == "..") currDir = currDir->parent;
+				else {
+					if (currDir->dirs[first] == nullptr) currDir->dirs[first] = new Directory(first, currDir);
+					currDir = currDir->dirs[first];
+				}
+			}
+		}
+		else if (first == "dir") {
+			f >> first;
+			if (currDir->dirs[first] == nullptr) currDir->dirs[first] = new Directory(first, currDir);
+		}
+		else {
+			f >> second;
+			currDir->files[second] = stoi(first);
+			currDir->increaseSize(stoi(first));
+		}
+		if (f.eof()) break;
+	}
+    Directory minDir = *(root);
+    int needed = 30000000 - (70000000 - (root->size));
+	sum(root, minDir, needed);
+    cout << minDir.size;
 }
