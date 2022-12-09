739609830a58437a0b71b4a7894f0a8c7c980b20
Arhum Jain <arhumjain10@gmail.com>
2022-12-08 20:42:43 -0800

    day 8

--- Day 8/treetopP1.cpp
+++ Day 8/treetopP1.cpp
@@ -1,20 +1,72 @@
 #include "../utils/advent.h"
-
+// could probably put all the repetitive code but too lazy too :(
 using namespace aoc;
 
-constexpr int TEST = 1;
+constexpr int TEST = 0;
 F f(TEST == 1 ? "test.in" : "main.in");
 
-vec2d<tup<int, bool>> grid;
+vec2d<pair<int, bool>> grid;
 int main() {
     string row;
     int r = 0;
+
     while(true) {
         f>>row;
-        grid.push_back(vec<tup<int, bool>>());
+        grid.push_back(vec<pair<int, bool>>());
         for (int i=0; i<row.size(); i++) {
-            grid[r][i] = tup<int, bool>((int) i, false);
+            grid[r].push_back(make_pair(row[i]-'0', false));
         }
+        r++;
         if (f.eof()) break;
     }
+    int count = (grid[0].size()*2) + (grid.size()*2) - 4;
+    int max;
+
+    for (int r=1; r<grid.size()-1; r++) {
+        max = grid[r][0].first;
+        for (int c=1; c<grid[r].size()-1; c++) {
+            if ((grid[r][c]).first > max) {
+                max = grid[r][c].first;
+                if (grid[r][c].second == false) {
+                    grid[r][c].second = true;
+                    count++;
+                }
+            }
+        }
+        max = grid[r][grid[0].size()-1].first;
+        for (int c=grid[r].size()-2; c > 0; c--) {
+            if ((grid[r][c]).first > max) {
+                max = grid[r][c].first;
+                if (grid[r][c].second == false) {
+                    grid[r][c].second = true;
+                    count++;
+                }
+            }
+        }
+    }
+
+    for (int c=1; c<grid[0].size()-1; c++) {
+        max = grid[0][c].first;
+        for (int r=1; r<grid.size()-1; r++) {
+            if ((grid[r][c]).first > max) {
+                max = grid[r][c].first;
+                if (grid[r][c].second == false) {
+                    grid[r][c].second = true;
+                    count++;
+                }
+            }
+        }
+        max = grid[grid.size()-1][c].first;
+        for (int r = grid.size()-2; r > 0; r--) {
+            if ((grid[r][c]).first > max) {
+                max = grid[r][c].first;
+                if (grid[r][c].second == false) {
+                    grid[r][c].second = true;
+                    count++;
+                }
+            }
+        }
+    }
+
+    cout << count;
 }
--- Day 8/treetopP2.cpp
+++ Day 8/treetopP2.cpp
@@ -1,10 +1,56 @@
 #include "../utils/advent.h"
-
 using namespace aoc;
 
-constexpr int TEST = 1;
+constexpr int TEST = 0;
 F f(TEST == 1 ? "test.in" : "main.in");
 
+vec2d<pair<int, bool>> grid;
 int main() {
+    string row;
+    int r = 0;
 
+    while(true) {
+        f>>row;
+        grid.push_back(vec<pair<int, bool>>());
+        for (int i=0; i<row.size(); i++) {
+            grid[r].push_back(make_pair(row[i]-'0', false));
+        }
+        r++;
+        if (f.eof()) break;
+    }
+    
+    int max = -1;
+    int score;
+    int count;
+    for (int r=0; r<grid.size(); r++) {
+        for (int c=0; c<grid[r].size(); c++) {
+            count = 0;
+            score = 1;
+            for (int ri = r-1; ri > -1; ri--) {
+                count++;
+                if (grid[ri][c].first >= grid[r][c].first) break;
+            }
+            score *= count;
+            count = 0;
+            for (int ri = r+1; ri<grid.size(); ri++) {
+                count++;
+                if (grid[ri][c].first >= grid[r][c].first) break;
+            }
+            score *= count;
+            count = 0;
+            for (int ci = c - 1; ci > -1; ci--) {
+                count++;
+                if (grid[r][ci].first >= grid[r][c].first) break;
+            }
+            score *= count;
+            count = 0;
+            for (int ci = c + 1; ci < grid[r].size(); ci++) {
+                count++;
+                if (grid[r][ci].first >= grid[r][c].first) break;
+            }
+            score *= count;
+            if (score > max) max = score;
+        }
+    }
+    cout << max << endl;
 }
