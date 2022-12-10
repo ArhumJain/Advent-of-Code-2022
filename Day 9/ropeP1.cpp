53f4e17617c3694c63ec7dbd987a833e24bf687c
Arhum Jain <arhumjain10@gmail.com>
2022-12-09 22:48:08 -0800

    Day 9

--- Day 9/ropeP1.cpp
+++ Day 9/ropeP1.cpp
@@ -2,35 +2,25 @@
 
 using namespace aoc;
 
-constexpr int TEST = 1;
+constexpr int TEST = 0;
 F f(TEST == 1 ? "test.in" : "main.in");
-
 
 map<pair<int, int>, bool> grid;
 umap<char, pair<int, int>> dirMap = {{'U', {0, 1}}, {'D', {0, -1}}, {'L', {-1, 0}}, {'R', {1, 0}}};
-
-int c = 0;
-char dir;
-int step;
-pair<int, int> hPos = {0, 0};
-pair<int, int> tPos = {0, 0};
-
+char dir; int step;
+pair<int, int> hPos, tPos = {0, 0};
 int main() {
     while (true) {
         f >> dir >> step;
-        if (f.eof()) break;
         for (int i=0; i<step; i++) {
-            hPos.first += dirMap[dir].first;
-            hPos.second += dirMap[dir].second;
-            if (abs(hPos.first-tPos.first) + abs(hPos.second-tPos.second) > 1) {
+            hPos.first += dirMap[dir].first; hPos.second += dirMap[dir].second;
+            if (max(abs(hPos.first-tPos.first), abs(hPos.second-tPos.second)) > 1) {
                 tPos.first = hPos.first - dirMap[dir].first;
-                tPos.second -= hPos.second - dirMap[dir].second;
-                if (grid[make_pair(tPos.first, tPos.second)] == false) {
-                    grid[make_pair(tPos.first, tPos.second)] = true;
-                    c++;
-                }
+                tPos.second = hPos.second - dirMap[dir].second;
+                grid[{tPos.first, tPos.second}] = true;
             }
         }
+        if (f.eof()) break;
     }
-    cout << c;
+    cout << grid.size()+1;
 }
--- Day 9/ropeP2.cpp
+++ Day 9/ropeP2.cpp
@@ -2,9 +2,28 @@
 
 using namespace aoc;
 
-constexpr int TEST = 1;
+constexpr int TEST = 0;
 F f(TEST == 1 ? "test.in" : "main.in");
 
+map<pair<int, int>, bool> grid;
+umap<char, pair<int, int>> dirMap = {{'U', {0, 1}}, {'D', {0, -1}}, {'L', {-1, 0}}, {'R', {1, 0}}};
+char dir; int step;
+pair<int,int> knots[10] = {pair<int,int>(0,0)};
 int main() {
-
+    while (true) {
+        f >> dir >> step;
+        for (int i=0; i<step; i++) {
+            knots[0].first += dirMap[dir].first; 
+            knots[0].second += dirMap[dir].second;
+            for (int k=1; k<10; k++) { 
+                if (max(abs(knots[k-1].first-knots[k].first), abs(knots[k-1].second-knots[k].second)) > 1) {
+                    if (knots[k].first != knots[k-1].first) knots[k].first += (knots[k-1].first-knots[k].first > 0 ? 1 : -1);
+                    if (knots[k].second != knots[k-1].second) knots[k].second += (knots[k-1].second-knots[k].second > 0 ? 1 : -1);
+                }
+            }
+            grid[{knots[9].first, knots[9].second}] = true;
+        }
+        if (f.eof()) break;
+    }
+    cout << grid.size();
 }
