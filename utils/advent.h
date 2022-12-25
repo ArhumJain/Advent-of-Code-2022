cdbd8e61f555c685d4433432eb8febe8594cc49e
ArhumJain <arhumjain10@gmail.com>
2022-12-25 02:54:41 -0800

    utils update and polish

--- utils/advent.h
+++ utils/advent.h
@@ -3,47 +3,48 @@
 using namespace std;
 
 namespace aoc {
-    using F = ifstream;
+    using F = std::ifstream;
 
     template <typename T>
-    using vec3d = vector<vector<vector<T>>>;
+    using vec3d = std::vector<std::vector<std::vector<T>>>;
 
     template <typename T>
-    using vec2d = vector<vector<T>>;
+    using vec2d = std::vector<std::vector<T>>;
 
     template<typename T>
-    using vec = vector<T>;
+    using vec = std::vector<T>;
 
     template<typename T1, typename T2>
-    using umap = unordered_map<T1, T2>;
+    using umap = std::unordered_map<T1, T2>;
 
     template<typename T>
-    using uset = unordered_set<T>;
+    using uset = std::unordered_set<T>;
 
     template<typename T1, typename T2>
-    using tup = tuple<T1, T2>;
+    using tup = std::tuple<T1, T2>;
 
     using ul = unsigned long;
     using ll = long long;
     using ull = unsigned long long;
 
-    using sstream = stringstream;
+    using sstream = std::stringstream;
 
     template <typename T> 
-    void print1d(vector<T> &v) {
-        for (T i: v) cout << i << endl;
+    void print1d(std::vector<T> &v, string delimiter="\n") {
+        for (T i: v) std::cout << i << delimiter;
     }
 
     template <typename T> 
-    void print2d(vector<vector<T>> &v) {
+    void print2d(std::vector<std::vector<T>> &v, string delimitColumn=" ", string delimitRow="\n") {
         for (auto _: v) {
-            for(T i: _) cout << i << " ";
-            cout << endl;
+            for(T i: _) std::cout << i << delimitColumn;
+            std::cout << delimitRow;
         }
     }
 
-    vector<string> split(string s, string d = " ") { // thanks stackoverflow!
-        vector<string> sp;
+
+    std::vector<std::string> split(std::string s, std::string d = " ") { // thanks stackoverflow!
+        std::vector<std::string> sp;
         int start, end = -1*d.size();
         do {
             start = end + d.size();
@@ -51,6 +52,26 @@ namespace aoc {
             sp.push_back(s.substr(start, end-start));
         } while (end != -1);
         return sp;
+    }
+
+    string read(std::ifstream &f) {
+        std::string r((std::istreambuf_iterator<char>(f)),
+                            (std::istreambuf_iterator<char>()));
+        return r;   
+    }
+
+    void readlines(std::ifstream &f, std::vector<std::string> &r) {
+        std::string _;
+        while (getline(f, _)) r.push_back(_);
+    }
+    
+    std::vector<std::string> readlines(std::ifstream &f) {
+        std::vector<std::string> lines;
+        std::string r;
+        while (getline(f, r)) {
+            lines.push_back(r);
+        }
+        return lines;
     }
 
 }
